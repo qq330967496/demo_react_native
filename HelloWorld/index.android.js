53b73da7193581d15e4b84662cf36b607588675b
shihua.he <330967496@qq.com>
2016-12-08 11:51:09 +0800

    测试提交

--- HelloWorld/index.android.js
+++ HelloWorld/index.android.js
@@ -1,38 +1,42 @@
 import React, { Component } from 'react';
-import { AppRegistry, Navigator, Text, View } from 'react-native';
-
-import MyScene from './js/MyScene';
+import { AppRegistry,StyleSheet, View } from 'react-native';
 
 class HelloWorld extends Component {
-  render() {
-    return (
-        <Navigator
-            initialRoute={{ title: 'My Initial Scene', index: 0 }}
-            renderScene={(route, navigator) =>
-                <MyScene
-                    title={route.title}
+    _onPressButton() {
+        console.log("You tapped the button!");
+    }
 
-                    // Function to call when a new scene should be displayed
-                    onForward={ () => {
-                      const nextIndex = route.index + 1;
-                      navigator.push({
-                        title: 'Scene ' + nextIndex,
-                        index: nextIndex,
-                      });
-                    }}
-
-                    // Function to call to go back to the previous scene
-                    onBack={() => {
-                      if (route.index > 0) {
-                        navigator.pop();
-                      }
-                    }}
-                />
-            }
-        />
-    )
-  }
+    render() {
+        return (
+            <TouchableHighlight onPress={this._onPressButton}>
+                <Text>Button</Text>
+            </TouchableHighlight>
+        );
+    }
 }
 
 
+
+
+
+
 AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
+
+
+/*
+//触摸事件
+class MyButton extends Component {
+    _onPressButton() {
+        console.log("You tapped the button!");
+    }
+
+    render() {
+        return (
+            <TouchableHighlight onPress={this._onPressButton}>
+                <Text>Button</Text>
+            </TouchableHighlight>
+        );
+    }
+}
+
+*/
